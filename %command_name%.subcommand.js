1285a3ba29d46a3465d407d360ef8ca89d8fbce2
baeonette <baeonette@gmail.com>
2022-08-22 17:47:16 -0700

    Update %command_name%.subcommand.js

--- %command_name%.subcommand.js
+++ %command_name%.subcommand.js
@@ -1,12 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('%command_name%')
-        .setDescription('%command_name% slash command')
-        .setDMPermission(false),
+async function %command_name%Subcommand(interaction) {
+    interaction.reply('%command_name% works!');
+}
 
-    async execute(interaction) {
-        interaction.reply({ content: "%command_name% works!", ephemeral: false });
-    }
-};
+module.exports = %command_name%Subcommand;
